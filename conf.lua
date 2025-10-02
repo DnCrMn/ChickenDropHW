c5b31bf0aac9963ae8462de8b58ee6e2044eb859
DnCrMn <dancarrsonmendoza@gmail.com>
2025-10-02 12:16:19 -0500

    Add original Chicken Drop assets and scripts

---
+++ conf.lua
@@ -0,0 +1,7 @@
+function love.conf(t)
+    t.title = "My Game" -- Window title
+    t.window.width = 800 -- Window width (default 800)
+    t.window.height = 600 -- Window height (default 600)
+    t.window.resizable = false -- dleth the window be user-resizable (default false)
+    t.window.vsync = 1 -- Vertical sync mode (default 1)
+end
